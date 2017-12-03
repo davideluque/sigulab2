19384f66ed07a6c948e174ce17a40dc5cde84289
Yari <yari.luciani@yahoo.com>
2017-12-03 18:24:59 -0400

    Arreglos

--- controllers/servicios.py
+++ controllers/servicios.py
@@ -689,6 +689,18 @@ def ajax_listado_solicitudes_generadas():
                 nextpage=nextpage, prevpage=prevpage,
                 firstpage=firstpage, lastpage=lastpage)
 
+
+#------------------------------------------------------------------------------
+#
+# Controladores de los reportes
+#
+#------------------------------------------------------------------------------
+
+@auth.requires_login(otherwise=URL('modulos', 'login'))
+def pdfSolicitud():
+    return dict()
+
+
 # Funcion para enviar un correo de notificacion 
 def __enviar_correo(destinatario, asunto, cuerpo):
     mail = auth.settings.mailer
