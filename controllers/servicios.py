f59e5baf3716d33f775dafcdd3a14cca7f7225d7
Donato <13-10173@usb.ve>
2017-12-04 23:03:29 -0400

    Privilegios de visualizacion de servicios ocultos

--- controllers/servicios.py
+++ controllers/servicios.py
@@ -641,7 +641,29 @@ def ajax_certificar_servicio():
 def ajax_listado_servicios():
 
     #----- LISTAR SERVICIOS -----#
-    listado_de_servicios = ListaServicios(db, auth.has_membership(group_id='WebMaster'))
+
+    # Conseguir dependencia y si su rol es suficiente para ver servicios ocultos
+
+    grupo_lab = db(db.auth_group.role == "Jefe de Laboratorio").select(db.auth_group.id)[0].id
+    grupo_dir = db(db.auth_group.role == "Director").select(db.auth_group.id)[0].id
+    grupo_asistdir = db(db.auth_group.role == "Asistente del Director").select(db.auth_group.id)[0].id
+    grupo_admin = db(db.auth_group.role == "WebMaster").select(db.auth_group.id)[0].id
+
+    info_membership = db(db.auth_membership.user_id == auth.user_id).select()[0]
+    user_group_id = info_membership.group_id
+
+    dependencia = info_membership.dependencia_asociada
+    print(dependencia)
+
+    rol = 0
+    if user_group_id == grupo_dir or user_group_id == grupo_asistdir or user_group_id == grupo_admin:
+        rol = 2
+    elif user_group_id == grupo_lab:
+        rol = 1
+
+    print(rol)
+
+    listado_de_servicios = ListaServicios(db, dependencia, rol)
 
     order_by_asc = eval(request.post_vars.ordenarAlfabeticamente.title())
     order_by_col = request.post_vars.ordenarPor
