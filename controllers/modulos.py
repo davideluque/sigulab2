9102f9c54aa31c8d90774e1e4f4654e7ae25bcb3
David Cabeza <cabezadavide@gmail.com>
2017-12-30 22:34:37 -0400

    Register at 75%

--- controllers/modulos.py
+++ controllers/modulos.py
@@ -7,7 +7,8 @@
   
 #sys.setdefaultencoding('utf8')
 
-import re
+from gluon.contrib.pbkdf2 import pbkdf2_hex
+from hashlib import sha512
 
 #-------------------------------------
 #
@@ -15,92 +16,123 @@ import re
 #
 #-------------------------------------
 
-# Pagina, botones de eleccion entre nuestros modulos
-# o SMDP
-
 @auth.requires_login(otherwise=URL('modulos', 'login'))
 def index():
-	return dict()
+  """
+  @description Pagina principal de elección de módulos SMyDP o Otros Módulos
+  """
+  return dict()
 
 @auth.requires_login(otherwise=URL('modulos', 'login'))
 def sigulab2():
-	return dict()
-
-#-------------------------------------
-# Autenticacion y manejo de cuenta
-# ToDo login
-# ToDo Mostrar errores mejor
-# ToDo Boton para volver a la pantalla de eleccion de modulos
-#
-# ToDo register
-# ToDo Validar, mientras se escribe, los campos (?)
-# ToDo Mostrar errores
-# ToDo Nombre y Apellido solo letras guiones espacios y caracteres especiales
-# ToDo Cambiar tamaño de los dropdowns
-# ToDo Boton para volver al login
-# ToDo Asegurar que se vea que cada campo es obligatorio (?)
-# ToDo correo de verificacion (?) + correo de aprobacion
-#
-# ToDo resetpassword
-# ToDo Boton para volver al Login
-# ToDo Mostrar errores mejor
-#
-# ToDo recoverpassword
-# ToDo Vistas enteras
-# ToDo Boton para ir al login
-# 
-#-------------------------------------
+  return dict()
 
 def authenticate():
+  """
+  @description Método de autenticación alternativo al formulario de Web2py.
+  Hecho mediante ajax.
 
-	if not 'token_send' in request.post_vars:
-		return "Esta conexión no es segura. Prueba entrando a la página de nuevo. \
-		Si continuas viendo este mensaje, contacta al administrador del sitio."
+  @returns Mensajes de error o un redirect HTML si la autenticación es
+  satisfactoria.
+  """
+  if not 'token_send' in request.post_vars:
+    return "Esta conexión no es segura. Prueba entrando a la página de nuevo. \
+    Si continúa viendo este mensaje, contacte al administrador del sitio."
 
-	if not '@' in request.post_vars.email_send:
-		return "Correo inválido. Formato: algo@ejemplo.com"
+  if not '@' in request.post_vars.email_send:
+    return "Correo inválido. Formato: algo@ejemplo.com"
 
-	user = auth.login_bare(request.post_vars.email_send, request.post_vars.pass_send)
-	
-	if not user:
-		return "Datos de inicio de sesión incorrectos."
-	else:
-		url = URL('index')
-		return '<meta http-equiv="refresh" content="0; url='+ url + '">'
+  user = auth.login_bare(request.post_vars.email_send, request.post_vars.pass_send)
+  
+  if not user:
+    return "Datos de inicio de sesión incorrectos."
+  else:
+    url = URL('index')
+    return '<meta http-equiv="refresh" content="0; url='+ url + '">'
 
-# Inicio de Sesion
 def login():
-	if auth.user:
-		return redirect(URL('index'))
+  """
+  @description Login obsoleto. Solo es utilizado para acceder a la página
+  como tal y redireccionar en caso de que el usuario ya fue autenticado.
 
-	form=auth.login()
-	
-	if request.vars['error'] == 'invalid_data':
-		return dict(form=form, error="Datos de inicio de sesión incorrectos")
-	
-	return dict(form=form, error=None)
+  @return Redirección a la página principal de elección de nuevos módulos
+  en caso de que el usuario ya fue autenticado.
+  """
+  if auth.user:
+    return redirect(URL('index'))
 
-# Perfil de Usuario
+  form=auth.login()
+  
+  if request.vars['error'] == 'invalid_data':
+    return dict(form=form, error="Datos de inicio de sesión incorrectos")
+  
+  return dict(form=form, error=None)
+
 @auth.requires_login(otherwise=URL('modulos', 'login'))
 def editprofile():
-	return dict(form=auth.profile(), form2=auth.change_password())
+  """
+  @description Página en donde cada usuario autenticado puede editar su perfil
+  y cambiar su contraseña.
 
-# Registro de usuarios- Incluye la creacion del rol
+  @returns form de edición de usuario autenticado, form de cambio de
+  contraseña de usuario autenticado
+  """
+  return dict(form=auth.profile(), form2=auth.change_password())
+
+def check_role():
+  """
+  @description Método que verifica el grupo del usuario que intenta acceder
+  a la página de registro.
+
+  @returns True en caso de que el usuario pertenezca a alguno de los grupos
+  autorizados a realizar registros en este sistema. False de lo contrario.
+  """
+
+  grupo_webmaster = db(db.auth_group.role == "WebMaster").select(db.auth_group.id)[0].id
+  grupo_director = db(db.auth_group.role == "Director").select(db.auth_group.id)[0].id
+  grupo_asistente_director = db(db.auth_group.role == "Asistente del Director").select(
+    db.auth_group.id)[0].id
+
+  auth_user_group_id = db(db.auth_membership.user_id == auth.user_id).select()[0].group_id
+
+  if (auth_user_group_id == grupo_webmaster) \
+   or (auth_user_group_id == grupo_director) \
+    or (auth_user_group_id == grupo_asistente_director):
+    return True
+  elif auth.user.email == 'ulab-calidad@usb.ve':
+    return True
+
+  return False
+
+@auth.requires(lambda: check_role())
 def register():
-	if auth.user:
-		return redirect(URL('index'))
-	form=auth.register()
-	roles=list(db(db.auth_group.role != 'WebMaster').select(db.auth_group.ALL))
-	return dict(form=form, roles=roles)
+  """
+  @description El registro de usuarios está habilitado únicamente para los 
+  administradores del sitio que son aquellos que pertecen a uno de los
+  siguientes grupos: Grupo Webmaster, grupo Director, grupo Asistente del 
+  Director o Coordinadora de la Calidad.
+  """
+
+  ### Realizar registro de usuario ###
+  if request.vars and request.vars.registrar == "do_register":
+    db.auth_user.insert(first_name=request.vars.first_name, 
+      last_name=request.vars.last_name, email=request.vars.email, 
+      password=pbkdf2_hex(request.vars.password, 'salt', iterations=1000, keylen=24, hashfunc=sha512))
+
+
+
+  roles=list(db(db.auth_group.role != 'WebMaster').select(db.auth_group.ALL))
+
+  return dict(roles=roles)
 
 def redireccionando():
-	user = db(db.auth_user.id > 0).select(db.auth_user.ALL)[-1]
-	
-	print(session.ci)
+  user = db(db.auth_user.id > 0).select(db.auth_user.ALL)[-1]
+  
+  print(session.ci)
 
-	db.auth_membership.insert(user_id=user, group_id=session.rolid, dependencia_asociada=session.depid, f_personal_membership = session.ci)
- 	
- 	db.t_Personal.insert(f_nombre = user.first_name,
+  db.auth_membership.insert(user_id=user, group_id=session.rolid, dependencia_asociada=session.depid, f_personal_membership = session.ci)
+  
+  db.t_Personal.insert(f_nombre = user.first_name,
                                 f_apellido = user.last_name,
                                 f_ci = session.ci,
                                 f_email = user.email,
@@ -112,65 +144,65 @@ def redireccionando():
                                 f_fecha_salida = "1/02/1989",
                                 f_dependencia = session.depid
                                 )
-	session.forget()
-	return redirect(URL('index'))
+  session.forget()
+  return redirect(URL('index'))
 
 # Ajax Helper para la dependencia de acuerdo a su unidad de adscripcion
 def ajax_unidad_rol():
-	rolid = request.post_vars.dependenciahidden
-	roltype = db(db.auth_group.id == int(rolid)).select(db.auth_group.ALL)[0].role
-	direccion=db(db.dependencias.nombre == "Dirección").select(db.dependencias.ALL)
-	labs_y_coordinaciones=list(db(db.dependencias.unidad_de_adscripcion == direccion[0].id).select(db.dependencias.ALL))
-	labs = []
-	coordinaciones = []
-	for i in labs_y_coordinaciones:
-		if "Laboratorio" in i.nombre:
-			labs.append(i)
-		else:
-			coordinaciones.append(i)
-	if roltype == "Director" or roltype == "Asistente del Director" or roltype == "Gestor de SMyDP":
-		lista = direccion
-	elif roltype == "Coordinador" or roltype == "Personal de Coordinación":
-		lista = coordinaciones
-	elif roltype == "WebMaster" or roltype == "Cliente Interno":
-		lista = False
-	else:
-		lista = labs
-	return(dict(lista=lista))
+  rolid = request.post_vars.dependenciahidden
+  roltype = db(db.auth_group.id == int(rolid)).select(db.auth_group.ALL)[0].role
+  direccion=db(db.dependencias.nombre == "Dirección").select(db.dependencias.ALL)
+  labs_y_coordinaciones=list(db(db.dependencias.unidad_de_adscripcion == direccion[0].id).select(db.dependencias.ALL))
+  labs = []
+  coordinaciones = []
+  for i in labs_y_coordinaciones:
+    if "Laboratorio" in i.nombre:
+      labs.append(i)
+    else:
+      coordinaciones.append(i)
+  if roltype == "Director" or roltype == "Asistente del Director" or roltype == "Gestor de SMyDP":
+    lista = direccion
+  elif roltype == "Coordinador" or roltype == "Personal de Coordinación":
+    lista = coordinaciones
+  elif roltype == "WebMaster" or roltype == "Cliente Interno":
+    lista = False
+  else:
+    lista = labs
+  return(dict(lista=lista))
 
 # Ajax helper para crear una membership para el usuario recien registrado
 def ajax_membership():
-	session.depid = None
-	session.rolid = int(request.post_vars.rol)
-	session.ci = int(request.post_vars.cedula)
-	print(request.post_vars.cedula)
-	if request.post_vars.laboratorio:
-		session.depid = int(request.post_vars.laboratorio)
-	if request.post_vars.seccion:
-		session.depid = int(request.post_vars.seccion)
-	return dict()
+  session.depid = None
+  session.rolid = int(request.post_vars.rol)
+  session.ci = int(request.post_vars.cedula)
+  print(request.post_vars.cedula)
+  if request.post_vars.laboratorio:
+    session.depid = int(request.post_vars.laboratorio)
+  if request.post_vars.seccion:
+    session.depid = int(request.post_vars.seccion)
+  return dict()
 
 # Ajax Helper para mostrar dependencias a Tecnicos y Jefes de seccion
 def ajax_registro_seccion(): 
-	rolid = request.post_vars.dependenciahidden
-	roltype = db(db.auth_group.id == int(rolid)).select(db.auth_group.ALL)[0].role
-	secciones=False
-	if roltype == "Técnico" or roltype == "Jefe de Sección":
-		labid = request.post_vars.seccionhidden
-		secciones=list(db(db.dependencias.unidad_de_adscripcion == int(labid)).select(db.dependencias.ALL))
+  rolid = request.post_vars.dependenciahidden
+  roltype = db(db.auth_group.id == int(rolid)).select(db.auth_group.ALL)[0].role
+  secciones=False
+  if roltype == "Técnico" or roltype == "Jefe de Sección":
+    labid = request.post_vars.seccionhidden
+    secciones=list(db(db.dependencias.unidad_de_adscripcion == int(labid)).select(db.dependencias.ALL))
 
-	return dict(lista=secciones)
+  return dict(lista=secciones)
 
 # Recuperacion de Contraseña (pedido) 
 def resetpassword():
-	site_url = URL('default', 'recoverpassword', host=True)
+  site_url = URL('default', 'recoverpassword', host=True)
 
-	user = db(db.auth_user.email == request.post_vars.email).select(db.auth_user.ALL)
-	
-	try:
-		user = user[0]
+  user = db(db.auth_user.email == request.post_vars.email).select(db.auth_user.ALL)
+  
+  try:
+    user = user[0]
 
-		auth.messages.reset_password = '<html>\
+    auth.messages.reset_password = '<html>\
   <head>\
     <title>Restablecer tu contraseña de SIGULAB</title>\
   </head>\
@@ -251,13 +283,13 @@ def resetpassword():
       </tr>\
     </table>\
   </body>\
- 	</html>'
-	except:
-		pass
+  </html>'
+  except:
+    pass
 
-	form = auth.request_reset_password()
-	return dict(form=form)
+  form = auth.request_reset_password()
+  return dict(form=form)
 
 def logout():
-	auth.logout()
-	return redirect(URL('login'))
+  auth.logout()
+  return redirect(URL('login'))
