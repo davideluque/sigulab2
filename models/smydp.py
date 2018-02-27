3d5c9ed977841730349ac07d14f8e691203487d9
saar1312 <saar1312@yahoo.com>
2018-02-27 06:38:59 +0000

    Agregando tabla de sustancias (catalogo/listado de sustancias)

--- models/smydp.py
+++ models/smydp.py
@@ -1,26 +1,28 @@
-#------------------------------------ Modulo de Sustancias, materiales y desechos -------------------------------------------
+#------------------------------------ Modulo de Sustancias, materiales y desechos peligrosos-------------------------------------------
 
 
-"""
 #t_Sustancia: Tabla de sustancias de la cual se obtiene informacion del listado (catalogo de sustancias)
 db.define_table(
     #Nombre de la entidad
     't_Sustancia', 
     #Atributos;
-    Field('f_nombre', 'string', label=T('Nombre'),requires=IS_NOT_EMPTY(),
-    Field('f_cas', 'string', label=T('CAS'),requires=IS_NOT_EMPTY()),
-    Field('f_pureza', 'integer',requires=IS_INT_IN_RANGE(0, 101), label=T('Pureza')),
-    Field('f_estado', 'integer', requires=IS_IN_DB(db,db.t_estado.id,'%(f_estado)s'), label=T('Estado'),
-    Field('f_control', 'integer', label=T('Control'), requires=IS_IN_DB(db,db.t_regimenes.id,'%(f_nombre)s'),
-    Field('f_peligrosidad', 'list:string', label=T('Peligrosidad'),requires=IS_IN_SET(['Inflamable','Tóxico','Tóxico para el ambiente','Corrosivo','Comburente','Nocivo','Explosivo','Irritante'],multiple = True),
-    widget=SQLFORM.widgets.checkboxes.widget),
-    Field('f_reporte','upload',label=T('MSDS'),requires=IS_NULL_OR(IS_UPLOAD_FILENAME(extension='pdf'))),
-    format='%(f_nombre)s',
-    migrate=settings.migrate)
+    Field('f_nombre', 	'string',	requires=IS_NOT_EMPTY(), label=T('Nombre')),
+    Field('f_cas', 		'string',	requires=IS_NOT_EMPTY(), unique=True, label=T('CAS')),
+    Field('f_pureza', 	'integer',	requires=IS_INT_IN_RANGE(0, 101), label=T('Pureza')),
+    Field('f_estado', 'list:string',requires=IS_IN_SET(['Sólido','Líquido','Gaseoso']), 
+    widget=SQLFORM.widgets.checkboxes.widget, label=T('Estado')),
+    Field('f_control', 'list:integer',requires=IS_IN_SET(['Ninguno','RL4','RL7', 'RL4 y RL7']), 
+    widget=SQLFORM.widgets.checkboxes.widget, label=T('Control')),
+    Field('f_peligrosidad', 'list:string',requires=IS_IN_SET(['Inflamable','Tóxico','Tóxico para el ambiente','Corrosivo','Comburente','Nocivo','Explosivo','Irritante'],multiple = True),
+    widget=SQLFORM.widgets.checkboxes.widget, label=T('Peligrosidad')),
+    # Hoja de seguridad (archivo pdf)
+    Field('f_hds','upload',requires=IS_NULL_OR(IS_UPLOAD_FILENAME(extension='pdf')),label=T('Hoja de seguridad')),
+    format='%(f_nombre)s')
 
-db.t_sustancias.id.readable=False
-db.t_sustancias.id.writable=False
-db.t_sustancias.f_reporte.readable=(auth.has_membership('Gestor de Sustancias')or auth.has_membership('WebMaster'))
-db.t_sustancias._singular='Listado de Sustancias'
-db.t_sustancias._plural='Listado de Sustancias'
-"""
+#db.t_sustancias.id.readable=False #Si se muestra en la forma, descomentar
+#db.t_sustancias.id.writable=False
+db.t_Sustancia.f_hds.readable=(auth.has_membership('Gestor de SMyDP') or auth.has_membership('WEBMASTER')) #-*-* Chequear permisos aqui
+db.t_Sustancia._singular='Catálogo de Sustancias'
+db.t_Sustancia._plural='Catálogo de Sustancias'
+
+
