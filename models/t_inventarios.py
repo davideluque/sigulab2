3558e93cadc85b0a0287a666db5598c2ab8f6dfb
root <sandrav0609@gmail.com>
2018-06-09 13:17:40 -0400

    Arreglado error de falta id en tablas de consumibles y materiales de lab. Error al tener mas de una clave

--- models/t_inventarios.py
+++ models/t_inventarios.py
@@ -44,83 +44,6 @@ db.bien_mueble.bm_depedencia.requires = IS_IN_DB(db, db.dependencias.id,'%(nombr
 
 #####No se implementa una tabla para datos de adquisicion#####
 
-### Tablas de categorias ###
-### Heredan de bien mueble
-
-#Maquinaria y demas equipos de construccion, campo, industria y taller
-db.define_table(
-    'maquinaria_construccion',
-    #db.bien_mueble,
-    Field('mc_NroBM', 'reference bien_mueble', unique=True, notnull=True, label = T('Número Bien Nacional')),
-    Field('mc_nombre','string', notnull=True,label=T('Maquinaria y demás equipos de construcción, campo, industria y taller')),
-    Field('mc_subcategoria','string',notnull=True,label=T('Subcategoría'),
-        requires=IS_IN_SET(['Maquinaria y equipos de construcción y mantenimiento','Maquinaria y equipos para mantenimiento de automotores','Maquinaria y equipos agrículas y pecuarios',
-            'Maquinaria y equipos de artes gráficas y reproducción','Maquinaria y equipos industriales y de taller','Maquinaria y equipos de energía',
-            'Maquinaria y equipos de riego y acueductos','Equipos de almacen','Otras maquinarias y demás equipos de construcción, campo, industria y taller']))
-    )
-#db.maquinaria_construccion.mc_NroBM.requires = IS_IN_DB(db, db.bien_mueble.id,'%(bm_num)s') 
-
-
-#Equipos de transporte, traccion y elevacion
-db.define_table(
-    'equipo_transporte',
-    #db.bien_mueble,
-    Field('et_NroBM', 'reference bien_mueble', unique=True, notnull=True, label = T('Número Bien Nacional')),
-    Field('et_nombre','string', notnull=True,label=T('Equipos de transporte, tracción y elevación')),
-    Field('et_subcategoria','string',notnull=True,label=T('Subcategoría'),
-        requires=IS_IN_SET(['Vehículos automotores y terrestes','Equipos ferroviarios y de cables aéreos','Equipos marítimos de transporte','Equipos aéreos de transporte',
-            'Vehículos de tracción no motorizados','Equipos auxiliares de transporte','Otros equipos de transporte, tracción y elevación']))
-    )
-#db.equipo_transporte.et_NroBM.requires = IS_IN_DB(db,db.bien_mueble.id,'%(bm_num)s')
-
-#Equipos de comunicaciones y de senalamiento
-db.define_table(
-    'equipo_comunicaciones',
-    #db.bien_mueble,
-    Field('ec_NroBM', 'reference bien_mueble', unique=True, notnull=True, label = T('Número Bien Nacional')),
-    Field('ec_nombre','string' ,notnull=True,label=T('Equipos de comunicaciones y de señalamiento')),
-    Field('ec_subcategoria','string',notnull=True,label=T('Subcategoría'),
-        requires=IS_IN_SET(['Equipos de telecomunicaciones','Equipos de señalamiento','Equipos de control de tráfico aéreo','Equipos de corrreo',
-            'Otros equipos de comunicaciones y de señalamiento']))
-    )
-#db.equipo_comunicaciones.ec_NroBM.requires = IS_IN_DB(db,db.bien_mueble.id,'%(bm_num)s') 
-
-#Equipos medicos-quirurgicos, dentales y veterinarios
-db.define_table(
-    'equipo_medico',
-    #db.bien_mueble,
-    Field('em_NroBM', 'reference bien_mueble', unique=True, notnull=True, label = T('Número Bien Nacional')),
-    Field('em_nombre','string', notnull=True,label=T('Equipos médicos-quirúrgicos, dentales y veterinarios')),
-    Field('em_subcategoria','string',notnull=True,label=T('Subcategoría'),
-        requires=IS_IN_SET(['Equipos médicos-quirúrgicos, dentales y veterinarios','Otros equipos médicos-quirúrgicos, dentales y veterinarios']))
-    )
-#db.equipo_medico.em_NroBM.requires = IS_IN_DB(db,db.bien_mueble.id,'%(bm_num)s') 
-
-#Equipos cientificos, religiosos, de ensenanza y recreacion
-db.define_table(
-    'equipo_cientifico_religioso',
-    #db.bien_mueble,
-    Field('ecr_NroBM', 'reference bien_mueble', unique=True, notnull=True, label = T('Número Bien Nacional')),
-    Field('ecr_nombre','string', notnull=True,label=T('Equipos científicos, religiosos, de enseñanza y recreación')),
-    Field('ecr_subcategoria','string',notnull=True,label=T('Subcategoría'),
-        requires=IS_IN_SET(['Equipos científicos y de laboratorio','Equipos de enseñanza, deporte y recreación','Obras de arte','Libros y revistas','Equipos religiosos',
-            'Instrumentos musicales','Otros equipos científicos, religiosos, de enseñanza y recreación']))    
-    )
-#db.equipo_cientifico_religioso.ecr_NroBM.requires = IS_IN_DB(db,db.bien_mueble.id,'%(bm_num)s') 
-
-
-#Maquinas, muebles y demas equiposde oficina y de alojamiento
-db.define_table(
-    'equipo_oficina',
-    #db.bien_mueble,
-    Field('eo_NroBM', 'reference bien_mueble', unique=True, notnull=True, label = T('Número Bien Nacional')),
-    Field('eo_nombre','string', notnull=True,label=T('Máquinas, muebles y demás equipos de oficina y de alojamiento')),
-    Field('eo_subcategoria','string',notnull=True,label=T('Subcategoría'),
-        requires=IS_IN_SET(['Mobiliario y equipos de oficina','Equipos de procesamiento de datos','Mobiliario y equipos de alojamiento',
-            'Otras máquinas, muebles y demás equipos de oficina y de alojamiento']))
-    )
-#db.equipo_oficina.eo_NroBM.requires = IS_IN_DB(db,db.bien_mueble.id,'%(bm_num)s') 
-
 ### Mantenimiento ###
 #Hay que preguntar cuales son obligatorios
 db.define_table(
@@ -241,6 +164,7 @@ db.solicitud_modificar_bien_mueble.modificar_NroBM.requires = IS_IN_DB(db,db.bie
 
 db.define_table(
 	'sin_bn',
+    Field('sb_id','id'),
 	Field('sb_nombre', 'string', notnull = True, label = T('Nombre del elemento')),
 	Field('sb_marca', 'string', label = T('Marca del elemento')),
 	Field('sb_modelo', 'string', label = T('Modelo/código del elemento')),
@@ -255,6 +179,7 @@ db.sin_bn.sb_espacio.requires = IS_IN_DB(db, db.espacios_fisicos.id,'%(nombre)s'
 
 db.define_table(
 	'consumible',
+    Field('co_id','id'),
 	Field('co_presentacion', 'string', notnull = True, label = T('Presentación')),
 	Field('co_unidades', 'string', notnull = True, label = T('Unidades por presentación'), requires = IS_MATCH('^[0-9]{5}$')),
 	Field('co_nombre', 'reference sin_bn', notnull = True, label = T('Nombre del elemento')),
@@ -263,11 +188,12 @@ db.define_table(
 	primarykey = ['co_nombre', 'co_espacio']
 	)
 
-db.consumible.co_espacio = IS_IN_DB(db, db.sin_bn.primarykey,'%(sb_espacio)s')
-db.consumible.co_nombre = IS_IN_DB(db, db.sin_bn.primarykey,'%(sb_nombre)s')
+db.consumible.co_espacio = IS_IN_DB(db, db.sin_bn.id,'%(sb_espacio)s')
+db.consumible.co_nombre = IS_IN_DB(db, db.sin_bn.id,'%(sb_nombre)s')
 
 db.define_table(
 	'material_laboratorio',
+    Field('ml_id','id'),
 	Field('ml_nombre', 'reference sin_bn', notnull = True, label = T('Nombre del elemento')),
 	Field('ml_espacio', 'reference sin_bn', notnull = True, label = T('Espacio fisico al que pertenece')),
 	Field('ml_aforado', 'string', notnull = True, label = T('Condición de aforado'), requires = IS_IN_SET('Si', 'No', 'N/A')),
